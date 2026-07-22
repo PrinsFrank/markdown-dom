8d6b4f36b8940153da7bc720de25fbd429fd45a4
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2026-07-22 19:19:42 +0200

    Add several inline and block nodes

---
+++ src/Node/Inline/Image.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace PrinsFrank\MarkDownDom\Node\Inline;
+
+use Override;
+use PrinsFrank\MarkDownDom\Contract\InlineNode;
+
+readonly class Image implements InlineNode {
+    public function __construct(
+        private string $text,
+        private string $href,
+    ) {}
+
+    #[Override]
+    public function __toString(): string {
+        return sprintf(
+            '![%s](%s)',
+            $this->text,
+            $this->href,
+        );
+    }
+}
